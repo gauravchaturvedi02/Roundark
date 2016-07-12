86ad6bcb688e1b4ebe28f32dd381ba86757aac9c
gauravchaturvedi02 <gaurav.pg007@gmail.com>
2016-07-12 18:34:57 +0530

    category page initial layout

---
+++ case-study.php
@@ -0,0 +1,113 @@
+
+<title>Case Study</title>
+<?php include 'header.php'; ?>
+
+<!--Case-Study page start -->
+<div class="top">
+<div class="container">
+<div class="master">
+<div class="case-study-title">
+<h2>Fnp wedding</h2>
+<!--<h5>Huge optimizes Gucci.com and brings luxury to mobile.</h5>-->
+</div>
+<div class="banner-case-studyproduct">
+<div id="main" role="main">
+      <div class="slider">
+        <div class="flexslider">
+          <ul class="slides">
+            <li><img src="images/banner-case-study.jpg" /></li>
+            <li><img src="images/banner-case-study.jpg" /></li>
+            <li><img src="images/banner-case-study.jpg" /></li>
+          </ul>
+        </div>
+      </div>
+      
+    </div>
+</div>
+</div>
+</div>
+</div>
+<div class="top case-study-bg">
+<div class="container">
+<div class="master">
+<div class="case-study-box">
+<div class="product-info">
+<p>Huge partnered with Gucci to optimize the international luxury brand's digital offering for mobile to boost sales and drive conversion on smartphones and tablets around the world</p>
+<p>With a larger, overall redesign on the horizon, there were two big constraints on the mobile project—we could only use existing code and photography, and it had to be done in two months Within these limits, the design and user experience challenge was to maintain a premium feel while also streamlining the purchase path; browsing, checking out and sharing favorite products on social channels all needed to be smooth and intuitive</p>
+<p>Applying learning from previous luxury e-commerce projects like Barneyscom, we made the products the heroes of every page, spotlighting big, high-resolution images and letting description and price fall below the fold  Knowing that Gucci customers (and luxury shoppers in general) tend to purchase only one expensive item at a time, we expedited the checkout process and condensed it into three simple steps  To customize the experience for global customers, we developed versions for multiple regions across Asia, Europe and the Americas, complying with the extensive region-specific rules governing online transactions (notably in Korea, Japan and Italy)  Finally, in order to facilitate repeat purchases, we made sure the mobile sites stored personal and billing information  We delivered all this for iOS and Android devices within the two-month window </p>
+</div>
+<div class="product-dedtails">
+<img src="images/case-study-product-name.png" alt="fnp" />
+<h3>Fernsnpetals</h3>
+<p>Year : <span>2013</span></p>
+<p>Categories : <span>Design, Ecommerce, User
+Experience</span></p>
+<a href="#">Share This</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="top brandimg-bg">
+<div class="container">
+<div class="master">
+<div class="brandimg">
+  <img src="images/brand-one-img.jpg" alt="banner" /></div>
+</div>
+</div>
+</div>
+<div class="top brand-detailsbg">
+<div class="container">
+<div class="master">
+<div class="brand-detail">
+<p>RoundArk was founded in 2006 as a result of a simple observation: people have transformed how they live, work, shop, and buy, but businesses have not adapted. This mismatch led Brian Halligan and Dharmesh Shah to create the vision for the inbound experience and to develop RoundArk's platform to support it. With our powerful, easy to use, integrated set of applications, businesses can attract, engage, and delight customers by delivering inbound experiences that are relevant, helpful, and personalized. RoundArk is, after all, on a mission to make the world more inbound, one business transformation after another.</p>
+</div>
+</div>
+</div>
+</div>
+<div class="top more-work-bg">
+<div class="container">
+<div class="master">
+<div class="more-work-row">
+<h3>More Work</h3>
+<ul>
+<li><a href="#">
+<span class="figure">
+<img src="images/nyc-thumb.jpg" alt="nyc" /> 
+<span class="center">
+
+</span>
+</span>
+<span class="proname">Maspar</span>
+</a>
+</li>
+<li><a href="#">
+<span class="figure">
+<img src="images/satyapaul.jpg" alt="nyc" /> 
+<span class="center">
+
+</span>
+</span>
+<span class="proname">Satyapaul</span>
+</a>
+</li>
+<li><a href="#">
+<span class="figure">
+<img src="images/nyc-thumb.jpg" alt="nyc" /> 
+<span class="center">
+
+</span>
+</span>
+<span class="proname">Maspar</span>
+</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+
+
+<!--Case-Study page end -->
+
+<?php include 'footer.php'; ?>
